380129a11afc9b38ffabad424dbde5599c727c35
LEanMist <leandro.chq123@gmail.com>
2025-11-05 10:14:24 -0300

    aprimoramento em get avaliaçoes

--- src/controllers/avaliação.controller.js
+++ src/controllers/avaliação.controller.js
@@ -5,7 +5,12 @@ import { db } from "../config/db.js"
 
 export async function listarAvaliacoes (req, res) {
     try {
-        const [rows] = await db.execute("SELECT * FROM avaliacoes");
+        const [rows] = await db.query(`
+            SELECT a.id, a.nota, a.comentario, u.nome AS usuario_nome, l.titulo AS livro_titulo
+            FROM avaliacoes a
+            JOIN usuarios u ON a.usuario_id = u.id
+            JOIN livros l ON a.livro_id = l.id
+        `);
         res.json(rows);
     }
     catch (err) {
@@ -18,6 +23,9 @@ export async function criarAvaliacao (req, res) {
         const { usuario_id, livro_id, nota, comentario } = req.body;
         if (!usuario_id || !livro_id || !nota || !comentario)       
             return res.status(400).json({ erro: "Campos obrigatórios" });
+        if (nota < 1 || nota > 10){
+            return res.status(400).json({ erro: "A nota deve estar entre 1 e 10" });
+        }
         await db.execute(
             "INSERT INTO avaliacoes (usuario_id, livro_id, nota, comentario) VALUES (?, ?, ?, ?)",
             [usuario_id, livro_id, nota, comentario]
