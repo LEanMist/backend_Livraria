aa47394052384ee96f8d4340e0b063111b08fc06
LEanMist <leandro.chq123@gmail.com>
2025-11-05 08:19:17 -0300

    3 rotas concluidas

--- src/controllers/avaliação.controller.js
+++ src/controllers/avaliação.controller.js
@@ -1,3 +1,31 @@
+import { db } from "../config/db.js"
+// ============================
+//  Rotas CRUD
+// ============================
 
+export async function listarAvaliacoes (req, res) {
+    try {
+        const [rows] = await db.execute("SELECT * FROM avaliacoes");
+        res.json(rows);
+    }
+    catch (err) {
+        res.status(500).json({ erro: err.message });
+    }
+};
+
+export async function criarAvaliacao (req, res) {
+    try {
+        const { id_usuario, id_livro, nota, comentario } = req.body;
+        if (!id_usuario || !id_livro || !nota || !comentario)       
+            return res.status(400).json({ erro: "Campos obrigatórios" });
+        await db.execute(
+            "INSERT INTO avaliacoes (id_usuario, id_livro, nota, comentario) VALUES (?, ?, ?, ?)",
+            [id_usuario, id_livro, nota, comentario]
+        );
+        res.json({ mensagem: "Avaliação criada com sucesso!" });
+    } catch (err) {
+        res.status(500).json({ erro: err.message });
+    }   
+};
 
 
